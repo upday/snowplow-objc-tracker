df7edbfd3b89a8d2bcfacea874c4f05dd5bd8b01
mhadam <michael@hadam.us>
2019-01-22 22:19:27 -0500

    hoping this fixes it

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,14 +6,14 @@ if [[ $PACKAGE_MANAGER == 'carthage' ]]; then
     xcodebuild -sdk iphonesimulator \
     -destination "${TEST_PLATFORM}" \
     -project Snowplow.xcodeproj \
-    -scheme 'Snowplow-iOS' \
+    -scheme Snowplow-iOS \
     clean test \
     | xcpretty
 elif [[ $PACKAGE_MANAGER == 'cocoapods' ]]; then
     xcodebuild -sdk iphonesimulator \
     -destination "${TEST_PLATFORM}" \
     -workspace Snowplow.xcworkspace \
-    -scheme 'Snowplow-iOS'\
+    -scheme Snowplow-iOS\
     clean test \
     | xcpretty
 fi
