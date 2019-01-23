22c39c7d3da0cd34b8e9907efddb862114b91a12
mhadam <michael@hadam.us>
2019-01-22 23:20:23 -0500

    Remove cocoapods testing

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-if [[ $PACKAGE_MANAGER == 'carthage' ]]; then
-    carthage update --platform iOS
-elif [[ $PACKAGE_MANAGER == 'cocoapods' ]]; then
-    pod repo update
-    pod install
-    pod update
-fi
+carthage update --platform iOS
--- .travis/test.sh
+++ .travis/test.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
 set -o pipefail
-if [[ $PACKAGE_MANAGER == 'carthage' ]]; then
-    # next command is broken up multiline for ease of editing
-    xcodebuild -sdk iphonesimulator \
-    -destination "${TEST_PLATFORM}" \
-    -project Snowplow.xcodeproj \
-    -scheme Snowplow-iOS \
-    clean test \
-    | xcpretty
-elif [[ $PACKAGE_MANAGER == 'cocoapods' ]]; then
-    xcodebuild -sdk iphonesimulator \
-    -destination "${TEST_PLATFORM}" \
-    -workspace Snowplow.xcworkspace \
-    -scheme Snowplow-iOS\
-    clean test \
-    | xcpretty
-fi
+
+xcodebuild -sdk iphonesimulator \
+-destination "${TEST_PLATFORM}" \
+-project Snowplow.xcodeproj \
+-scheme Snowplow-iOS \
+clean test \
+| xcpretty
