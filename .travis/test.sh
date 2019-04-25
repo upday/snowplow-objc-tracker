4f81aeb890ff9fa99a6e23b41d3aca5ac9778f90
Michael Hadam <michael@hadam.us>
2019-04-25 11:29:55 -0400

    Temporarily remove time-consuming tests

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,13 +2,6 @@
 
 set -o pipefail
 
-xcodebuild -sdk iphonesimulator \
-	-destination "${TEST_PLATFORM}" \
-	-project Snowplow.xcodeproj \
-	-scheme Snowplow-iOS \
-	clean test \
-	| xcpretty
-
 xcodebuild archive -project ./SnowplowSwiftDemo/SnowplowSwiftDemo.xcodeproj \
 	-scheme SnowplowSwiftDemo \
 	-archivePath ./build/ \
