24b9cbf89e6e5a912acdae3072fbf9ecddb8d6dc
Michael Hadam <michael@hadam.us>
2019-04-25 10:26:18 -0400

    Integrate Appium

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,8 +3,14 @@
 set -o pipefail
 
 xcodebuild -sdk iphonesimulator \
--destination "${TEST_PLATFORM}" \
--project Snowplow.xcodeproj \
--scheme Snowplow-iOS \
-clean test \
-| xcpretty
+	-destination "${TEST_PLATFORM}" \
+	-project Snowplow.xcodeproj \
+	-scheme Snowplow-iOS \
+	clean test \
+	| xcpretty
+
+xcodebuild archive -project ./SnowplowSwiftDemo/SnowplowSwiftDemo.xcodeproj \
+	-scheme SnowplowSwiftDemo \
+	-archivePath ./build/ \
+	-allowProvisioningUpdates \
+	| xcpretty
